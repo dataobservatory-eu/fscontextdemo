c780a53ea4048ce345172495c2e3893d3dce1266
Daniel Antal <antaldaniel@gmail.com>
2026-05-25 16:03:11 +0200

    Initial fscontextdemo package setup

---
+++ R/hello_world.R
@@ -0,0 +1,20 @@
+#' Return a friendly greeting
+#'
+#' A minimal example function used in the
+#' `fscontextdemo` package to demonstrate:
+#'
+#' - package structure
+#' - roxygen2 documentation
+#' - testing workflows
+#' - Git provenance reconstruction
+#'
+#' @return
+#' A character string containing a friendly greeting.
+#'
+#' @examples
+#' hello_world()
+#'
+#' @export
+hello_world <- function() {
+  "Hello world!"
+}
---
+++ data-raw/package_initialisation.R
@@ -0,0 +1,21 @@
+
+
+usethis::create_package(".")
+
+usethis::use_description(
+  fields = list(
+    Title = "Minimal Demonstration Package for fscontext Workflows",
+    Description = paste(
+      "A small self-contained R package used to demonstrate,",
+      "test, and document provenance-aware filesystem",
+      "reconstruction workflows with fscontext."
+    ),
+    URL = "https://github.com/dataobservatory-eu/fscontextdemo",
+    BugReports = "https://github.com/dataobservatory-eu/fscontextdemo/issues"
+  )
+)
+
+usethis::use_gpl3_license()
+
+
+usethis::use_readme_rmd()
